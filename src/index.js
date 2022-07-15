82695f984eb594a155bec02a4517fd6d3c4de527
Andrew Dodonov <nordhummer@gmail.com>
2022-07-15 23:15:17 +0300

    init: init commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let ResultArr = [];
+  let openedBrackets = [];
+  let closedBrackets = [];
+
+  bracketsConfig.forEach((item) => {
+    openedBrackets.push(item[0]);
+    closedBrackets.push(item[1]);
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    if (closedBrackets.includes(str[i]) && ResultArr.length !== 0) {
+      let secondBracket = openedBrackets[closedBrackets.indexOf(str[i])];
+      if (ResultArr[ResultArr.length - 1] === secondBracket || ResultArr[ResultArr.length - 1] === str[i]) {
+        ResultArr.pop();
+      } else if (str[i] !== secondBracket) {
+        return false;
+      } else {
+        ResultArr.push(str[i]);
+      }
+    } else {
+      ResultArr.push(str[i]);
+    }
+  }
+  return ResultArr.length === 0;
+};
